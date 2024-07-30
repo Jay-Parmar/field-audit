274dee26f58901894c2fcf342a8a599eb4eb0e17
Jay Parmar <jayparmar789@gmail.com>
2024-07-30 13:37:19 +0530

    Boilerplate for auditing app

---
+++ apps/openapi/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from apps.openapi import views
+
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+urlpatterns = [
+    
+]
